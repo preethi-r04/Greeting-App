c9471bad886602e4d6704a8ecb9a50ff2b041a95
preethi-r04 <preethiliki2004@gmail.com>
2026-03-11 15:21:30 +0530

    [Preethi]: Added: UC6: Get All Greeting Messages

--- src/main/java/com/bridgelabz/greeting_app/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greeting_app/service/GreetingService.java
@@ -5,6 +5,8 @@ import com.bridgelabz.greeting_app.repository.GreetingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class GreetingService {
 
@@ -32,7 +34,12 @@ public class GreetingService {
 
         return greetingRepository.save(greeting);
     }
+
     public Greeting getGreetingById(Long id) {
         return greetingRepository.findById(id).orElse(null);
+    }
+
+    public List<Greeting> getAllGreetings() {
+        return greetingRepository.findAll();
     }
 }
